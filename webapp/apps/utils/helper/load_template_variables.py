dee6062f184d4fda38b1a39b1fe88875737783a9
Colin Xie <colinjianingxie@gmail.com>
2023-01-09 14:56:53 -0500

    Added changes to how symbols loaded

--- webapp/apps/utils/helper/load_template_variables.py
+++ webapp/apps/utils/helper/load_template_variables.py
@@ -1,5 +1,8 @@
+from securities_master.models import DailyPrice
 from securities_master.models import Symbol
 
 def load_all_symbols(request):
-    symbols = Symbol.objects.all().order_by('ticker')
+
+    daily_price_id = [dpid['symbol_id'] for dpid in DailyPrice.objects.values('symbol_id').distinct()]
+    symbols = Symbol.objects.filter(id__in=daily_price_id)
     return {'symbols': symbols}
