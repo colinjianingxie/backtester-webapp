0de2a7cdddf68bd333c4e28d112d445886f995f6
Colin Xie <colinjianingxie@gmail.com>
2023-01-10 22:18:35 -0500

    Added barebones for obtaining parameters

---
+++ webapp/apps/api/views/post_backtest_strategy_parameters.py
@@ -0,0 +1,18 @@
+from api.views.helper import api_response
+from api.views.helper import ResponseStatus
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class PostBacktestStrategyParameterView(APIView):
+    """
+    """
+    def post(self, request):
+
+        strategy = request.POST['strategy']
+        context = self.strategy_parameter_helper(strategy)
+        return render(request, "partials/dashboard/components/modals/paramater_select_modal/modal.html", context)
+
+    def strategy_parameter_helper(self, strategy):
+        context = {}
+
+        return context
