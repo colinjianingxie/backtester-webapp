16810503e6f41035365f4bf9c9d919145ad3c9b9
Colin Xie <colinjianingxie@gmail.com>
2023-01-13 16:01:06 -0500

    Refactor

--- webapp/apps/dashboard/views/backtest_history.py
+++ webapp/apps/dashboard/views/backtest_history.py
@@ -1,6 +1,8 @@
 from braces.views import GroupRequiredMixin
 from braces.views import LoginRequiredMixin
 from django.views.generic import TemplateView
+from main.models import Backtest
+from main.models import BacktestResult
 from oauth.constants import UserGroup
 # User Access
 # Helpers
@@ -10,3 +12,11 @@ class BacktestHistoryView(LoginRequiredMixin, GroupRequiredMixin,TemplateView):
     raise_exception = True
     redirect_unauthenticated_users = True
     template_name = "apps/dashboard/backtest_history.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user_backtest_results = BacktestResult.objects.all().filter(backtest__account=self.request.user).order_by('-created_date')
+
+        context['backtest_results'] = user_backtest_results
+        return context
