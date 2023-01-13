1ccb51fe343c52aa71a69bc456b4e14d3fb71d38
Colin Xie <colinjianingxie@gmail.com>
2023-01-13 12:03:14 -0500

    Added basic templates

--- webapp/apps/dashboard/urls.py
+++ webapp/apps/dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .views import BacktestHistoryView
 from .views import BacktestResultsView
 from .views import BacktestView
 from .views import DashboardView
+from .views import ExploreView
+from .views import ForumView
 from .views import portal_redirect
 
 urlpatterns = [
@@ -11,9 +14,9 @@ urlpatterns = [
     ################################################
     path("login_redirect_success/",portal_redirect.login_redirect_success,name="login_redirect_success"),
     path('', DashboardView.as_view(), name='dashboard'),
-    #path('analysis_settings/', analysis_settings.AnalysisSettingsView.as_view(), name='analysis_settings'),
-    #path('analysis/analysis_id=<str:analysis_id>', analysis.AnalysisView.as_view(), name='analysis'),
-    #path('analysis/', analysis.AnalysisView.as_view(), name='analysis_redirect'),
+    path('forum/', ForumView.as_view(), name='forum'),
+    path('explore/', ExploreView.as_view(), name='explore'),
     path('backtest/', BacktestView.as_view(), name='backtest'),
+    path('backtest_history/', BacktestHistoryView.as_view(), name='backtest_history'),
     path('backtest_result/<str:backtest_id>/<str:backtest_result_id>', BacktestResultsView.as_view(), name='backtest_result'), # Need ID for backtest results
 ]
--- webapp/apps/dashboard/views/__init__.py
+++ webapp/apps/dashboard/views/__init__.py
@@ -1,3 +1,6 @@
 from .backtest import BacktestResultsView
 from .backtest import BacktestView
+from .backtest_history import BacktestHistoryView
 from .dashboard import DashboardView
+from .explore import ExploreView
+from .forum import ForumView
